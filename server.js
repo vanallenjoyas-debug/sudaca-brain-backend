46e171d612c21c3bf579af8df7c147fb9e7fb66d
vanallenjoyas-debug <vanallenjoyas@gmail.com>
2026-05-21 01:59:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const path = require('path');
 
-const VERSION = '1.9.3';
+const VERSION = '1.9.4';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const upload = multer({ dest: '/tmp/', limits: { fileSize: 100 * 1024 * 1024 } });
